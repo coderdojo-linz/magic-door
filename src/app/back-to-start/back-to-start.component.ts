157445b7e8a9dba6627cba937d064685f5793146
Rainer Stropek <rainer@software-architects.at>
2019-08-26 20:37:12 +0200

    Add runtime files

--- src/app/back-to-start/back-to-start.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-back-to-start',
-  templateUrl: './back-to-start.component.html',
-  styleUrls: ['./back-to-start.component.scss']
-})
-export class BackToStartComponent {
-}
--- src/app/wizard/wizard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-wizard',
-  templateUrl: './wizard.component.html',
-  styleUrls: ['./wizard.component.scss']
-})
-export class WizardComponent {
-}
